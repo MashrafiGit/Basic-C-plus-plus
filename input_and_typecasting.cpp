34a0bb9d605bfdc1e71d294f1e158435865858fd
Masrafi404 <mdmasrafi902@gmail.com>
2025-05-25 03:48:16 +0600

    input and typecasting in cpp

---
+++ input_and_typecasting.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    char y;
+    float z;
+
+    // int ascii = 'a';
+
+    cin >> x >> y >> z;
+    cout << (char)x << " " << (int)y << " " << (char)z << " " << endl;
+    return 0;
+}
