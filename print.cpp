9b840ccc79c6635d47fedd0ba28bacb872da009c
Masrafi404 <mdmasrafi902@gmail.com>
2025-05-25 03:35:47 +0600

    print in cpp

---
+++ print.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main()
+{
+    int x = 100;
+    char y = 'A';
+    double z = 45.24;
+
+    std::cout << x << " " << y << std::endl
+              << z << std::endl;
+
+    return 0;
+}
