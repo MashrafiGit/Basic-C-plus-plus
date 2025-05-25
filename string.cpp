3eb37145a2a9909f46a7c361a443366dfba2a002
Masrafi404 <mdmasrafi902@gmail.com>
2025-05-25 19:58:25 +0600

    string

---
+++ string.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+
+    string s;
+    cin >> s;
+
+    // char s[100];
+    // cin.ignore(); // ignore one input
+    // cin >> s;
+    
+    // get whole sentence
+    // fgets(s, 100, stdin);
+    // cin.getline(s, 100);
+
+
+
+    cout << x << " " << s << endl;
+
+    return 0;
+}
