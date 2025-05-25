b63a017a0c2f149e379ddc85f64eabc596308715
Masrafi404 <mdmasrafi902@gmail.com>
2025-05-25 16:16:00 +0600

    control fraction by set precision

---
+++ setPrecision.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    float x;
+    cin >> x;
+
+    cout << fixed << setprecision(5) << x << endl;
+
+    return 0;
+}
